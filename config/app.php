2cbe809f01471ff2ab8002a303696d41ddab036b
José Cage <josejanuario7@gmail.com>
2016-09-11 09:56:19 +0100

    Adicionando pacote ArtisanView para criar views com o comando artisan

--- config/app.php
+++ config/app.php
@@ -153,6 +153,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        Sven\ArtisanView\ArtisanViewServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -207,6 +209,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
