a42d1bf504efeba5fac8f39ff27a2a477be423b8
José Cage <josejanuario7@gmail.com>
2016-11-22 08:23:19 +0000

    Optimizando a automação de teste com Travis

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -14,12 +14,12 @@ class UsersSeeder extends Seeder
         $this->command->info('Usuários criados com sucesso!');
 
         DB::table('users')->delete();
-        //        DB::table('users')->truncate();
+        DB::table('users')->truncate();
         DB::table('users')->insert(
             [
-            ['name' => 'José Cage', 'email' => 'josecage@doesangueapp.dev', 'username' => 'josecage', 'bio' => 'Analista de Sistemas', 'password' => bcrypt('1234567')],
-            ['name' => 'Jó Cage', 'email' => 'jocage@doesangueapp.dev', 'username' => 'jocage', 'bio' => 'Desenvolvedor web', 'password' => bcrypt('1234567')],
-            ['name' => 'User Demo', 'email' => 'demo@doesangueapp.dev', 'username' => 'userdemo', 'bio' => 'Test Driver', 'password' => bcrypt('1234567')],
+            ['name' => 'José Cage', 'email' => 'josecage@doesangueapp.dev', 'username' => 'josecage', 'password' => bcrypt('1234567')],
+            ['name' => 'Jó Cage', 'email' => 'jocage@doesangueapp.dev', 'username' => 'jocage', 'password' => bcrypt('1234567')],
+            ['name' => 'User Demo', 'email' => 'demo@doesangueapp.dev', 'username' => 'userdemo', 'password' => bcrypt('1234567')],
             ]
         );
     }
