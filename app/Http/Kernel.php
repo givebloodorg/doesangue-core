f9a63aad184cc5812ed6ad33fde1a625286e59f8
José Cage <josejanuario7@gmail.com>
2017-05-27 17:23:07 +0100

    :fire: Disable csrf middleware verification

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \DoeSangue\Http\Middleware\VerifyCsrfToken::class,
+//            \DoeSangue\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
