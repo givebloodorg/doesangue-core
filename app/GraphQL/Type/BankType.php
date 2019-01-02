f98f4a33013e6f0a050477356e62c3eb7c01f4a0
José Cage <josejanuario7@gmail.com>
2019-01-02 01:09:31 +0100

    Removed GraphQL

---
+++ app/GraphQL/Type/BankType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DoeSangue\GraphQL\Type;
+
+class BankType
+{
+  //
+  protected $attributes = [
+    'name' => 'Blood Bank',
+    'description' => 'Blood Banks who joined us'
+  ];
+
+  public function fields()
+  {
+    return [
+      ''
+    ];
+  }
+}
--- app/GraphQL/Type/BanksType.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace DoeSangue\GraphQL\Type;
-
-class BanksType
-{
-  //
-}
